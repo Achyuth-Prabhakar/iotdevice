d54a6f328c6a6d6c978cecf4c1e9dedfe26bf7d7
Achyuth-Prabhakar <46367805+Achyuth-Prabhakar@users.noreply.github.com>
2021-05-26 01:35:02 +0530

    pubdata.py

--- pubdata.py
+++ pubdata.py
@@ -1,25 +1,6 @@
 import wiotp.sdk.device
 import time
 import random
-import smtplib
-from email.message import EmailMessage
-
-def email_alert(subject, body, to):
-    msg = EmailMessage()
-    msg.set_content(body)
-    msg['subject'] = subject
-    msg['to'] = to
-
-    user = "XXXXX sender_mail XXXXX"
-    msg['from'] = user
-    password = "XXX password_after_setting_up_two-factor-auth XXXXXX"
-
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(user, password)
-    server.send_message(msg)
-    server.quit()
-
 
 myConfig = { 
     "identity": {
@@ -43,8 +24,7 @@ while True:
     temp=random.randint(0,50)
     hum=random.randint(0,50)
     duration=random.randint(1,5)
-    if duration == 5 :
-        email_alert ("ALERT", "You have been standing in the radiation for too long", "XXXXXXX res_mail XXXXX")
+   
     uSvh=round(random.uniform(0,1),2)
     cpm=round(random.uniform(1,5),2)
     myData={'temperature':temp, 'humidity':hum,'duration':duration, 'uSvh':uSvh, 'cpm':cpm}
